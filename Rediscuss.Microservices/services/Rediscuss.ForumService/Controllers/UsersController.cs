6268ec8b101e16a3661019fd74b4ee1c0e357f03
Seyit Bulat <mseyit68@gmail.com>
2025-07-21 10:48:01 +0300

    RoleDto Olusturuldu. Kullanici Rol Listelemesi, Ekleme ve Cikartmasi Yazildi

--- Rediscuss.Microservices/services/Rediscuss.ForumService/Controllers/UsersController.cs
+++ Rediscuss.Microservices/services/Rediscuss.ForumService/Controllers/UsersController.cs
@@ -19,6 +19,103 @@ namespace Rediscuss.ForumService.Controllers
             _context = context;
         }
 
+
+        [HttpPost("{userId}/roles)")]
+        public async Task<IActionResult> AssignRoleToUser(int userId, string roleName)
+        {
+            var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(!int.TryParse(userIdString, out userId)) { return Unauthorized("Kullanıcı Bulunamadı"); }
+
+
+
+            var role = _context.Roles.Find(s => s.RoleName == roleName).FirstOrDefault();
+            
+            if(role == null) { return BadRequest("Rol Bulunamadı"); }
+
+            var isExituserRole = _context.UserRoles.Find(u => u.UserId == userId && u.RoleId == role.Id && u.IsDeleted == false).Any();
+
+            if(isExituserRole) { return BadRequest("Kullanıcın bu rolü var"); }
+
+            var userRole = new UserRole
+            {
+                UserId = userId,
+                RoleId = role.Id,
+                IsDeleted = false,
+                CreatedBy = userId
+            };
+
+            await _context.UserRoles.InsertOneAsync(userRole);
+
+            return Ok("Kullanıcıya Rol Atandı");
+        }
+
+
+        [HttpGet("{targetUserId}/roles")]
+        public async Task<IActionResult> GetUserRoles(int targetUserId)
+        {
+            int userId;
+            var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(userIdString, out userId)) { return Unauthorized("Kullanıcı Bulunamadı"); }
+
+            var userRoles = await _context.UserRoles.Find(u => u.UserId == targetUserId && u.IsDeleted == false).ToListAsync();
+
+            var roleIds = userRoles.Select(u => u.RoleId).ToList();
+
+            if (roleIds.Count == 0) { return Ok(new List<RoleDto> ()); }
+
+            var filter = Builders<Role>.Filter.In(r => r.Id, roleIds) & Builders<Role>.Filter.Eq(s => s.IsDeleted, false);
+
+            var roles = await _context.Roles.Find(filter).ToListAsync();
+
+            var roleDtos = roles.Select(r => new RoleDto { 
+                Id = r.Id,
+                RoleName = r.RoleName,
+                Scope = r.Scope,
+                Description = r.Description,
+                Permissions = r.Permissions.Select(p => new PermissionDto { Id = p.Id, ActionName = p.ActionName, Description = p.Description}).ToList(),
+                CreatedBy = userId
+            });
+
+            return Ok(roleDtos);
+        }
+
+
+        [HttpDelete("{targetUserId}/roles/{roleName}")]
+        public async Task<IActionResult> DeleteUserRole(int targetUserId, string roleName)
+        {
+            int userId;
+            var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(userIdString, out userId)) { return Unauthorized("Kullanıcı Bulunamadı"); }
+
+            var role = _context.Roles.Find(r => r.RoleName == roleName).FirstOrDefault();
+
+            if(role == null) { return BadRequest("Bu rol bulunamadı"); }
+
+            var userRoleFilter = Builders<UserRole>.Filter.And(
+                    Builders<UserRole>.Filter.Eq(u => u.UserId, targetUserId),
+                    Builders<UserRole>.Filter.Eq(u => u.RoleId, role.Id),
+                    Builders<UserRole>.Filter.Eq(u => u.IsDeleted, false)
+                );
+
+            var userRole = _context.UserRoles.Find(userRoleFilter).FirstOrDefault();
+
+            if(userRole == null) { return BadRequest("Kullanıcı bu role sahip değil"); }
+
+            var update = Builders<UserRole>.Update
+                .Set(u => u.IsDeleted, true)
+                .Set(u => u.DeletedAt, DateTime.UtcNow)
+                .Set(u => u.DeletedBy, userId);
+
+
+            await _context.UserRoles.UpdateOneAsync(userRoleFilter, update);
+
+            return Ok("Kullanıcı Rolü başarıyla silindi.");
+        }
+
+
         [HttpGet("me/subscriptions")]
         public async Task<IActionResult> GetMySubscriptions()
         {
