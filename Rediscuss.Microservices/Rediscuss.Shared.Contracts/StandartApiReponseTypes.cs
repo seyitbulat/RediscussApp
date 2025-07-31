5878b75950d6498b9084b2b91e13d492ab3d2242
Seyit Bulat <mseyit68@gmail.com>
2025-07-31 17:55:20 +0300

    JSON:API yapısına uygun bir şekilde StandartApiResponse classları oluşturuldu

---
+++ Rediscuss.Microservices/Rediscuss.Shared.Contracts/StandartApiReponseTypes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Rediscuss.Shared.Contracts
+{
+	public class StandartApiResponse<T>
+	{
+		[JsonPropertyName("name")]
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public T Data { get; set; }
+
+		[JsonPropertyName("errors")]
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public List<ApiError> Errors { get; set; }
+
+		[JsonPropertyName("links")]
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Dictionary<string, string> Links { get; set; }
+
+
+		[JsonPropertyName("meta")]
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+		public Dictionary<string, object> Meta { get; set; }
+
+
+		public static StandartApiResponse<T> Success(T data, Dictionary<string, string> links) => new() { Data = data, Links = links };
+
+		public static StandartApiResponse<T> Fail(List<ApiError> errors) => new() { Errors = errors };
+    }
+
+
+	public class JsonApiResource<TAttributes>
+	{
+		[JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+		[JsonPropertyName("attributes")]
+        public TAttributes Attributes { get; set; }
+    }
+
+	public class ApiError
+	{
+		[JsonPropertyName("status")]
+		public string Status { get; set; }
+
+		[JsonPropertyName("title")]
+		public string Title { get; set; }
+
+		[JsonPropertyName("detail")]
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public string Detail { get; set; }
+	}
+}
